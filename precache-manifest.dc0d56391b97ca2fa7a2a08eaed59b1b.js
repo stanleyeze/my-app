8318b0cd7f244db34a9a85a64c9951e64a4c6a12
kenstufsz <stanleyekene.ze@gmail.com>
2020-01-13 04:16:44 -0600

    hello

---
+++ precache-manifest.dc0d56391b97ca2fa7a2a08eaed59b1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e61d39d2829f47961ee23fce338cc8b1",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a234b0fac69258e40056",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d2410e3029b2951164ed",
+    "url": "/my-app/static/js/2.f97d9805.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.f97d9805.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a234b0fac69258e40056",
+    "url": "/my-app/static/js/main.623efc99.chunk.js"
+  },
+  {
+    "revision": "bdfba45c951e28cd6616",
+    "url": "/my-app/static/js/runtime-main.7f6384da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
